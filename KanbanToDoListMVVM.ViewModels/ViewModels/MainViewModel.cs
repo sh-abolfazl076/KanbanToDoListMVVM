7ad844205bf4e2eaab3cd12cde7217ba5cd51315
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-22 19:44:30 +0330

    Create MainViewModel class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/ViewModels/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KanbanToDoListMVVM.ViewModels.ViewModels
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public ViewModelBase CurrentViewModel { get; set; }
+        public MainViewModel() 
+        {
+            CurrentViewModel = new LoginViewModel();
+        }
+    }
+}
