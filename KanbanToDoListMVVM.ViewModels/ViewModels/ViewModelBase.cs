c1a659589ec892ed4346a993c45d0dbf6819eaf8
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-22 19:30:14 +0330

    Create ViewModelBase class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/ViewModels/ViewModelBase.cs
@@ -0,0 +1,20 @@
+﻿// System
+using System.ComponentModel;
+
+
+// Internal
+
+
+
+namespace KanbanToDoListMVVM.ViewModels.ViewModels
+{
+    public class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged(string propertyChanged)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyChanged));
+        }
+    }
+}
