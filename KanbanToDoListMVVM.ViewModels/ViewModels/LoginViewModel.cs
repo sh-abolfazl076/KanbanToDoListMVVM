82d32fc333b712c9e95993ddc9c4742c88b801d8
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-22 19:56:48 +0330

    Create LoginViewModel class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/ViewModels/LoginViewModel.cs
@@ -0,0 +1,56 @@
+﻿// System
+using System.Windows.Input;
+
+
+
+// Internal
+
+
+
+namespace KanbanToDoListMVVM.ViewModels.ViewModels
+{
+    public class LoginViewModel : ViewModelBase
+    {
+        private string _txtUsername;
+        public string TxtUsername
+        {
+            get
+            {
+                return _txtUsername;
+            }
+            set
+            {
+                _txtUsername = value;
+                OnPropertyChanged(nameof(TxtUsername));
+            }
+        }
+        ////
+       
+        private string _txtPassword;
+        public string TxtPassword
+        {
+            get
+            {
+                return _txtPassword;
+            }
+            set
+            {
+                _txtPassword = value;
+                OnPropertyChanged(nameof(TxtPassword));
+            }
+        }
+        ////
+        
+        public ICommand SubmitConnection { get; }
+        public ICommand SubmitSingup { get; }
+        public ICommand SubmitLogin { get; }
+
+        public LoginViewModel()
+        {
+
+        }
+
+
+
+    }
+}
