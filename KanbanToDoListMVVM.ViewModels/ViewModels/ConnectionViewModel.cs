92cc9f8f3993de3d7eadeb66e464fbba3d94801e
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-23 15:40:36 +0330

    Create ConnectionViewModel class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/ViewModels/ConnectionViewModel.cs
@@ -0,0 +1,79 @@
+﻿// System
+using System.Windows.Input;
+
+
+// Internal
+
+namespace KanbanToDoListMVVM.ViewModels.ViewModels
+{
+    public class ConnectionViewModel :ViewModelBase
+    {
+        private string _serverNameDatabase;
+        public string ServerNameDatabase
+        {
+            get
+            {
+                return _serverNameDatabase;
+            }
+            set
+            {
+                _serverNameDatabase = value;
+                OnPropertyChanged(nameof(ServerNameDatabase));
+            }
+        }
+        ////
+
+        private string _databaseName;
+        public string DatabaseName
+        {
+            get
+            {
+                return _databaseName;
+            }
+            set
+            {
+                _databaseName = value;
+                OnPropertyChanged(nameof(DatabaseName));
+            }
+        }
+        ////
+        
+        private string _usernameDatabase;
+        public string UsernameDatabase
+        {
+            get
+            {
+                return _usernameDatabase;
+            }
+            set
+            {
+                _usernameDatabase = value;
+                OnPropertyChanged(nameof(UsernameDatabase));
+            }
+        }
+        ////
+
+        private string _passwordDatabase;
+        public string PasswordDatabase
+        {
+            get
+            {
+                return _passwordDatabase;
+            }
+            set
+            {
+                _passwordDatabase = value;
+                OnPropertyChanged(nameof(PasswordDatabase));
+            }
+        }
+        ////
+
+        public ICommand ButtonCheckConnection {  get;}
+
+        public ConnectionViewModel()
+        {
+
+        }
+
+    }
+}
