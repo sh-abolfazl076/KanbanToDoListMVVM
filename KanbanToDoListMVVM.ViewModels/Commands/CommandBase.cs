688c04d771188e11dd92bd84ebf6abe610890487
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-24 10:02:30 +0330

    Create CommandBase class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/Commands/CommandBase.cs
@@ -0,0 +1,27 @@
+﻿// System
+using System;
+using System.Windows.Input;
+
+
+// Internal
+
+namespace KanbanToDoListMVVM.ViewModels.Commands
+{
+    public abstract class CommandBase : ICommand
+    {
+        public event EventHandler CanExecuteChanged;
+
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public abstract void Execute(object parameter);
+        
+        public void OnCanExecuteChanged()
+        {
+            CanExecuteChanged?.Invoke(this, EventArgs.Empty);
+        }
+
+    }
+}
