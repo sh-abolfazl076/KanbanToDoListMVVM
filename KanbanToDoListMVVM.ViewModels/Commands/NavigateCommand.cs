d6699d591263b100d7871dc0739f0f93068e532a
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-24 10:42:24 +0330

    Create NavigateCommand class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/Commands/NavigateCommand.cs
@@ -0,0 +1,28 @@
+﻿// System
+using System;
+
+
+// Internal
+using KanbanToDoListMVVM.ViewModels.Stores;
+using KanbanToDoListMVVM.ViewModels.ViewModels;
+
+
+namespace KanbanToDoListMVVM.ViewModels.Commands
+{
+    public class NavigateCommand<TViewModle> : CommandBase where TViewModle : ViewModelBase
+    {
+        private readonly NavigationStore _navigationStore;
+        private readonly Func<TViewModle> _createViewModle;
+
+        public NavigateCommand(NavigationStore navigationStore , Func<TViewModle> createViewModle)
+        {
+            _navigationStore = navigationStore;
+            _createViewModle = createViewModle;
+        }
+
+        public override void Execute(object parameter)
+        {
+            _navigationStore.CurrentViewModel = _createViewModle();
+        }
+    }
+}
