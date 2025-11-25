de14e3cdd1971ae8bb01c71f550d2f519d047d58
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-25 18:23:59 +0330

    Create MainPanleViewModel class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/ViewModels/MainPanleViewModel.cs
@@ -0,0 +1,51 @@
+﻿// System
+using System;
+using System.Windows.Input;
+
+// Internal
+
+
+namespace KanbanToDoListMVVM.ViewModels.ViewModels
+{
+    public class MainPanleViewModel : ViewModelBase
+    {
+        private string _txtUsername;
+        public string TxtUsername
+        {
+            get
+            {
+                return _txtUsername;
+            }
+            set
+            {
+                _txtUsername = value;
+                OnPropertyChanged(nameof(TxtUsername));
+            }
+        }
+        ////
+
+        private string _txtPassword;
+        public string TxtPassword
+        {
+            get
+            {
+                return _txtPassword;
+            }
+            set
+            {
+                _txtPassword = value;
+                OnPropertyChanged(nameof(TxtPassword));
+            }
+        }
+        ////
+
+        public ICommand ButtomUsersList { get; }
+        public ICommand ButtomLogOut { get; }
+        public ICommand ButtomCreateTask { get; }
+
+        public MainPanleViewModel() 
+        {
+
+        }
+    }
+}
