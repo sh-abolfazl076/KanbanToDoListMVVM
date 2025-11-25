89badbb9882dd0c66d669b75710361f6351da8a8
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-25 15:22:14 +0330

    Create SingUpViewModel class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/ViewModels/SingupViewModel.cs
@@ -0,0 +1,49 @@
+﻿// System
+
+// Internal
+using System.Windows.Input;
+
+namespace KanbanToDoListMVVM.ViewModels.ViewModels
+{
+    public class SingUpViewModel :ViewModelBase
+    {
+        private string _txtUsernameSingUp;
+        public string TxtUsernameSingUp
+        {
+            get
+            {
+                return _txtUsernameSingUp;
+            }
+            set
+            {
+                _txtUsernameSingUp = value;
+                OnPropertyChanged(nameof(TxtUsernameSingUp));
+            }
+        }
+        ////
+
+        private string _txtPasswordSingUp;
+        public string TxtPasswordSingUp
+        {
+            get
+            {
+                return _txtPasswordSingUp;
+            }
+            set
+            {
+                _txtPasswordSingUp = value;
+                OnPropertyChanged(nameof(TxtPasswordSingUp));
+            }
+        }
+        ////
+
+        public ICommand SubmitAddUser{ get; }
+        public ICommand ButtonClose{ get; }
+
+        public SingUpViewModel()
+        {
+
+        }
+
+    }
+}
