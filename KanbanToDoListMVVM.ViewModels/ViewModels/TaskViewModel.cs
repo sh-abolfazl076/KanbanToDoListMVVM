8901f1accd18b243b824eb8012602aceecde6d64
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-26 16:26:04 +0330

    Create TaskViewModel class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/ViewModels/TaskViewModel.cs
@@ -0,0 +1,52 @@
+﻿// System
+
+// Internal
+
+
+using System.Windows.Input;
+
+namespace KanbanToDoListMVVM.ViewModels.ViewModels
+{
+    public class TaskViewModel : ViewModelBase
+    {
+        private string _txtTitleTask;
+        public string TxtTitleTask
+        {
+            get
+            {
+                return _txtTitleTask;
+            }
+            set
+            {
+                _txtTitleTask = value;
+                OnPropertyChanged(nameof(TxtTitleTask));
+            }
+        }
+        ////
+        
+        private string _txtInfoTask;
+        public string TxtInfoTask
+        {
+            get
+            {
+                return _txtInfoTask;
+            }
+            set
+            {
+                _txtInfoTask  = value;
+                OnPropertyChanged(nameof(TxtInfoTask));
+            }
+        }
+        ////
+
+        public ICommand EditButtom{ get; }
+        public ICommand ColseButtom { get; }
+        public ICommand RemoveButtom { get; }
+
+        public TaskViewModel()
+        {
+
+        }
+
+    }
+}
