966c8a5973b1c6dd90a6aee85f32bd15e34a5566
A.ShahMohammadi <A.ShahMohammadi@DESKTOP-011KG65>
2025-11-27 16:02:35 +0330

    Create PermissionUserViewModel class in ViewModels

---
+++ KanbanToDoListMVVM.ViewModels/ViewModels/PermissionUserViewModel.cs
@@ -0,0 +1,97 @@
+﻿// System
+using System.Windows.Input;
+
+// Internal
+
+
+
+namespace KanbanToDoListMVVM.ViewModels.ViewModels
+{
+    public class PermissionUserViewModel : ViewModelBase
+    {
+
+        private string _titlePermission;
+        public string TitlePermission
+        {
+            get
+            {
+                return _titlePermission = "User Permission";
+            }
+            set
+            {
+                _titlePermission = value;
+                OnPropertyChanged(nameof(TitlePermission));
+            }
+        }
+        ////
+        
+        private bool _addTask;
+        public bool AddTask
+        {
+            get
+            {
+                return _addTask;
+            }
+            set
+            {
+                _addTask = value;
+                OnPropertyChanged(nameof(AddTask));
+            }
+        }
+        ////
+
+        private bool _deleteTask;
+        public bool DeleteTask
+        {
+            get
+            {
+                return _deleteTask;
+            }
+            set
+            {
+                _deleteTask = value;
+                OnPropertyChanged(nameof(DeleteTask));
+            }
+        }
+        ////
+
+        private bool _manageUserAccess;
+        public bool ManageUserAccess
+        {
+            get
+            {
+                return _manageUserAccess;
+            }
+            set
+            {
+                _manageUserAccess = value;
+                OnPropertyChanged(nameof(ManageUserAccess));
+            }
+        }
+        ////
+
+        private bool _updateTask;
+        public bool UpdateTask
+        {
+            get
+            {
+                return _updateTask;
+            }
+            set
+            {
+                _updateTask = value;
+                OnPropertyChanged(nameof(UpdateTask));
+            }
+        }
+        ////
+
+
+        public ICommand SubmitPermssionUser { get; }
+
+        public PermissionUserViewModel()
+        {
+            SubmitPermssionUser = new PermissionUserCommand();
+        }
+
+    }
+}
